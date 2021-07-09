9c8767bb7ae64d9c3c24a9ac6e610f1345967277
walidhkhan <walidhkhan@gmail.com>
2021-07-09 13:35:22 -0700

    completed quiz home page & styles added

--- script.js
+++ script.js
@@ -14,3 +14,61 @@ buildQuiz();
 // show results when submit button is pressed 
 submitButton.addEventListener("click", showResults);
 
+// quiz questions
+var quizQuestions = [
+    {
+    // 1st quiz question
+    question: "Commonly used data types in JavaScript DO NOT include:",
+    asnwers: {
+        a: "strings",
+        b: "booleans",
+        c: "alerts",
+        d: "numbers"
+    },
+    correctAnswer: "c"
+},
+    {
+    // 2nd quiz question
+    question: "The condition in an if / else statement is enclosed with ________.",
+    asnwers: {
+        a: "quotes",
+        b: "curly brackets",
+        c: "parenthesis",
+        d: "square brackets"
+    },
+    correctAnswer: "c"
+},
+    {
+    // 3rd quiz question
+    question: "Arrays in JavaScript can be used to store ________.",
+    asnwers: {
+        a: "numbers and strings",
+        b: "other arrays",
+        c: "booleans",
+        d: "all of the above"
+    },
+    correctAnswer: "c"
+},
+    {
+    // 4th quiz question
+    question: "String values must be enclosed within ________ when being assigned to variables",
+    asnwers: {
+        a: "commas",
+        b: "curly brackets",
+        c: "quotes",
+        d: "parenthesis"
+    },
+    correctAnswer: "c"
+},
+    {
+    // 5th quiz question
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    asnwers: {
+        a: "JavaScript",
+        b: "terminal/bash",
+        c: "for loops",
+        d: "console.log"
+    },
+    correctAnswer: "c"
+    }
+];
