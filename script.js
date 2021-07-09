c5e1b3b630c13675ffc9b26144031f569165d74b
walidhkhan <walidhkhan@gmail.com>
2021-07-09 11:37:44 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var quizContainer = document.getElementById("quiz");
+var resultsContainer = document.getElementById("results");
+var submitButton = document.getElementById("submit");
+
+// function to build quiz
+function buildQuiz(){}
+
+// function to show results
+function showResults(){}
+
+// to display the quiz
+buildQuiz();
+
+// show results when submit button is pressed 
+submitButton.addEventListener("click", showResults);
+
