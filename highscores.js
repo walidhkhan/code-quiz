eb6ceb8fe399a58599292a35e759501a8f629146
walidhkhan <walidhkhan@gmail.com>
2021-07-11 22:45:04 -0700

    commit for project evaluation

--- highscores.js
+++ highscores.js
@@ -1,24 +1,18 @@
-// highscores part 2 start--------------------------------------------------------------------
-
 let highScoresList = document.querySelector("#highScoresList");
 let highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 
 
-// highScoresList.innerHTML = 
-// highScores.map(score => {
-//     return `<li class="high-score">${score.name} - ${score.score}</li>`
-// }).join("")
-// highscores part 2 end-------------------------------------------------------------------------
-
-// highscores part 1 start---------------------------------------------------------------------
+highScoresList.innerHTML = 
+highScores.map(score => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`
+}).join("")
 
 let initials = document.querySelector("#initials");
 let saveScoreBtn = document.querySelector("#saveScoreBtn");
 let finalScore = document.querySelector("#finalScore");
 let mostRecentScore = localStorage.getItem("mostRecentScore");
-// -------------------
-let highscores = JSON.parse(localStorage.getItem("highscores")) || [];
+
 
 let MAX_HIGH_SCORES = 100;
 
@@ -26,41 +20,37 @@ finalScore.innerText = mostRecentScore;
 
 saveHighScore = e => {
     e.preventDefault();
-alert('here2');
+    // create score object
     let score = {
         score: mostRecentScore,
         name: initials.value
     }
+    // initialize high scores 
+    var highscores = [];
+    highscores = JSON.parse(localStorage["highScores"]);
 
+    // insert most recent score
     highscores.push(score);
 
+    // sort scores
     highscores.sort((a,b) => {
         return b.score - a.score;
     })
 
-    highscores.splice(5);
+    // max limit of high scores
+    highscores.splice(10);
 
-
-
+    // setting new array in localStorage
     localStorage.setItem("highScores", JSON.stringify(highscores))
-    alert('here3');
-
-    window.location.assign("")
-    alert('here4');
-
-
-    let highscores = JSON.parse(localStorage.getItem("highscores")) || [];
-    alert('hereFINAL');
-        highScoresList.innerHTML = 
-    highScores1.map(score => {
+    
+    // displaying all scores including most recent 
+    highScoresList = document.querySelector("#highScoresList");
+    highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+    highScoresList.innerHTML = 
+    highScores.map(score => {
         return `<li class="high-score">${score.name} - ${score.score}</li>`
     }).join("")
+
     
-    // for (i=0; i < highScores1.length; i++) {
-    //     var score1 = highScores1[i];
-    //     alert(score1.score);
-    // }
     
 }
-
-// highscores part 1 end--------------------------------------------------------------------------
